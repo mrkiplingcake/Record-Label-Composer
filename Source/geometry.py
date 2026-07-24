f124f1a37b1074117ab41c6650194579a78286a3
mrkiplingcake <loonix21@gmail.com>
2026-07-24 15:19:13 +0100

    Replace working area percentage with millimetre calculation

--- Source/geometry.py
+++ Source/geometry.py
@@ -6,19 +6,32 @@ geometry.py
 Geometry helper functions used throughout the application.
 """
 
-from constants import WORKING_DIAMETER_MM
+from constants import WORKING_DIAMETER_MM, WORKSPACE_DIAMETER_MM
 
 
 class LabelGeometry:
     """Geometry calculations for the workspace."""
 
     @staticmethod
-    def get_working_diameter_pixels(canvas):
-        """Returns the working radius of the label."""
+    def get_workspace_size_pixels(canvas):
+        """Returns the usable workspace size in pixels."""
         width = canvas.winfo_width()
         height = canvas.winfo_height()
 
-        return min(width, height) * 0.76
+        return min(width, height)
+
+    @staticmethod
+    def get_workspace_pixels_per_mm(canvas):
+         """Returns the number of pixels per millimetre in the workspace."""
+         workspace = LabelGeometry.get_workspace_size_pixels(canvas)
+
+
+         return workspace / WORKSPACE_DIAMETER_MM
+
+    @staticmethod
+    def get_working_diameter_pixels(canvas):
+        pixels_per_mm = LabelGeometry.get_workspace_pixels_per_mm(canvas)
+        return WORKING_DIAMETER_MM * pixels_per_mm
 
     @staticmethod
     def get_working_radius_pixels(canvas):
